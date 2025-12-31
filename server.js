7fbf8723366134db7775b2945a1ece88707d3947
Ravi75750 <mrbadshaff@gmail.com>
2025-12-31 16:14:05 +0530

    Add debug logging

--- server.js
+++ server.js
@@ -35,6 +35,12 @@ app.use(
 );
 app.use(express.json());
 
+/* DEBUG LOGGING */
+app.use((req, res, next) => {
+  console.log(`🔍 [LOG] ${req.method} ${req.url}`);
+  next();
+});
+
 /* STATIC FILES */
 /* STATIC FILES (Local Dev Only - Vercel uses Object Storage usually) */
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
